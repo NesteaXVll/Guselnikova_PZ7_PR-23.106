ed476e0c76db0124aeafc678dfe0c08cb14eb199
Anastasia <thedevil_2@mail.ru>
2024-04-24 19:26:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/guselnikova_pz7_pr_23106/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.guselnikova_pz7_pr_23106;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
